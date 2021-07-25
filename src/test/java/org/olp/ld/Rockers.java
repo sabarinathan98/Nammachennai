a9ea9738d52fff8e4bd2066f540da03e4de74a20
sabarinathan98 <sabarinathan4028@gmail.com>
2021-07-25 22:46:12 +0530

    Task has been completed

---
+++ src/test/java/org/olp/ld/Rockers.java
@@ -0,0 +1,12 @@
+package org.olp.ld;
+
+public class Rockers {
+	public static void main(String[] args) {
+		System.out.println("dd");
+		System.out.println("Ddd");
+		System.out.println("rwerwer");
+		
+	}
+
+}
+
